8cf77df3fa34fe195ff79183a1bc2b91da454fbd
LLeeJr <legralisandro@gmail.com>
2022-06-16 21:15:34 +0200

    feat: create dockerfile with given data

--- src/index.js
+++ src/index.js
@@ -2,7 +2,32 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
-const {ProbotOctokit} = require("probot");
+// const {ProbotOctokit} = require("probot");
+const fs = require("fs");
+const yaml = require('js-yaml');
+const { execSync } = require('child_process');
+
+function getInstallationToken() {
+  // TODO
+  return "token";
+}
+
+function createDockerfile(yaml, token, branch, owner, repoName) {
+  let dockerfile = `FROM node:16-slim\n`; // `FROM ${yaml.ci.language}`;
+  dockerfile += "RUN apt-get update\nRUN apt-get install git\n"
+  dockerfile += `RUN git clone --branch ${branch} https://x-access-token:${token}@github.com/${owner}/${repoName}.git\n`
+
+  for (let step of yaml.ci.steps) {
+    dockerfile += `RUN ${step}\n`;
+  }
+
+  fs.mkdirSync(`./Dockerfiles/${repoName}`, { recursive: true}, (err) => {
+    app.log.error(err);
+  });
+
+  fs.writeFileSync(`./Dockerfiles/${repoName}/Dockerfile`, dockerfile);
+}
+
 module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
@@ -15,43 +40,54 @@ module.exports = (app) => {
   });
 
   app.on(["pull_request.opened", "pull_request.synchronize", "pull_request.reopened"] , async (context) => {
-    await createCommitStatus(context, context.payload.pull_request.head.sha, "pending", "fetch yaml from branch", "custom-ci");
+    const headSha = context.payload.pull_request.head.sha;
+    const branch = context.payload.pull_request.head.ref;
+
+    await createCommitStatus(context, headSha, "pending", "fetch yaml from branch", "custom-ci");
 
     // Get yaml file from pr branch and decode it
     const yaml = await context.octokit.rest.repos.getContent({
       owner: context.repo().owner,
       repo: context.repo().repo,
       path: 'ci_cd.yml',
-      ref: context.payload.pull_request.head.ref
+      ref: branch
     })
         .then(result => readYaml(result.data.content))
         .catch(error => error.name);
 
-    app.log.info(`After fetching yaml: ${yaml}`);
+    app.log.info(`After fetching yaml: ${yaml.toString()}`);
 
     // differentiate between two errors
     // one for failing to fetch yaml from repo
     if (yaml === "HttpError") {
-      await createCommitStatus(context, context.payload.pull_request.head.sha, "error", "failed fetching yaml from this branch", "custom-ci");
+      await createCommitStatus(context, headSha, "error", "failed fetching yaml from this branch", "custom-ci");
       return;
     // the other for failing to decoding or returning data as yaml
     } else if (yaml === "ValidationError") {
-      await createCommitStatus(context, context.payload.pull_request.head.sha, "error", "failed validating yaml", "custom-ci");
+      await createCommitStatus(context, headSha, "error", "failed validating yaml", "custom-ci");
       return;
     }
 
-    await createCommitStatus(context, context.payload.pull_request.head.sha, "success", "successfully fetched yaml", "custom-ci");
+    // TODO validation of yaml
+
+    await createCommitStatus(context, headSha, "success", "successfully fetched and validated yaml", "custom-ci");
 
     // Check if a provider was chosen if not use own ci tool
     if (yaml.ci.provider === undefined) {
       app.log.info("chose custom ci");
+
+      // Create Dockerfile with given data
+      let token = getInstallationToken()
+
+      createDockerfile(yaml, token, branch, context.repo().owner, context.repo().repo);
+
       return;
     }
 
     // Check which provider should be triggered for ci
     if (yaml.ci.provider === "github-actions") {
       app.log.info("chose github-actions")
-      await createCommitStatus(context, context.payload.pull_request.head.sha, "pending", "starting github actions workflow", "custom-ci/github-actions");
+      await createCommitStatus(context, headSha, "pending", "starting github actions workflow", "custom-ci/github-actions");
       // TODO maybe multiple workflows?
       await githubActionsCI(context, yaml.ci.workflow_file_name);
     }
@@ -69,15 +105,15 @@ module.exports = (app) => {
   });
 };
 
-function callCommand() {
-  const { execSync } = require('child_process');
+// callCommand('docker build -f ci/node.Dockerfile -t test .').then(() => {
+//   console.info("Docker build done")
+// }).catch(error => console.error(error));
 
+async function callCommand(command) {
   return execSync('docker -v && git --version').toString();
 }
 
 function readYaml(content) {
-  const yaml = require('js-yaml');
-
   // Remove all line breaks
   const withoutLineBreaks = content.replace(/\n/g, "");
 
